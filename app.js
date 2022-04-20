c32e91ba60b059c51470003619694fee9b778305
somanshusingh <teamspybit@gmail.com>
2022-04-20 11:36:55 +0530

    allowed cors

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const config = require("config");
 const bodyParser = require("body-parser");
 const util = require("util");
 const fs = require("fs");
+const cors = require('cors');
+
 // get SQL DB driver connection
 //const db = require("./models/db");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors()); // used to avoid cors error
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
