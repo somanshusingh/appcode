6afb96ada69a95a9dc3dd6be89f2d4f2fd2711b6
somanshusingh <teamspybit@gmail.com>
2022-05-12 12:02:10 +0530

    add

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors()); // used to avoid cors error
 
 app.use(function (req, res, next) {
-  //res.header("Access-Control-Allow-Origin", "http://localhost:5500");
-  res.header("Access-Control-Allow-Origin", "*");
+  //res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://jbm.herokuapp.com");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
