8621e54165faafea810cacb4ed302a6e66e3d807
somanshusingh <teamspybit@gmail.com>
2022-05-15 00:12:59 +0530

    add

--- controllers/histories.js
+++ controllers/histories.js
@@ -12,7 +12,7 @@ module.exports.controller = function (app) {
   app.post('/history/inhouse_transport', (req, res)=>{
       try{
         let sql =
-            `CREATE TABLE ${tableName}(Trip_No VARCHAR(15) NOT NULL, VehicleNo VARCHAR(15), Material_Type VARCHAR(25),Material VARCHAR(50), Issued_By VARCHAR(50), Issued_Date DATE, Driver_Name VARCHAR(50), Driver_Number BIGINT, Time VARCHAR(15), Consignee_Name VARCHAR(50), Address VARCHAR(150), Gross_Weight DOUBLE, Tare_Weight DOUBLE, Net_Weight DOUBLE, Vehicle_Mapping VARCHAR(15), Qty_Mt_Weight DOUBLE, Status VARCHAR(10), LrNumber VARCHAR(10), LrDate DATE, Card_Number VARCHAR(20), Gross_Wgh_Date_time DATE, Tare_Wgh_Date_time DATE, Gate_In_Date_time DATE, Gate_Out_Date_time DATE. Remark_Field VARCHAR(200), Front_image json, Rear_Image json, PRIMARY KEY(Trip_No))`;
+            `CREATE TABLE ${tableName}(Trip_No VARCHAR(15) NOT NULL, VehicleNo VARCHAR(15), Material_Type VARCHAR(25),Material VARCHAR(50), Issued_By VARCHAR(50), Issued_Date DATE, Driver_Name VARCHAR(50), Driver_Number BIGINT, Time VARCHAR(15), Consignee_Name VARCHAR(50), Address VARCHAR(150), Gross_Weight DOUBLE, Tare_Weight DOUBLE, Net_Weight DOUBLE, Vehicle_Mapping VARCHAR(15), Qty_Mt_Weight DOUBLE, Status VARCHAR(10), LrNumber VARCHAR(10), LrDate DATE, Card_Number VARCHAR(20), Gross_Wgh_Date_time DATE, Tare_Wgh_Date_time DATE, Gate_In_Date_time DATE, Gate_Out_Date_time DATE, Remark_Field VARCHAR(200), Front_image json, Rear_Image json, PRIMARY KEY(Trip_No))`;
           db.query(sql, (err) => {
             try {
               if (err) {
@@ -134,7 +134,7 @@ module.exports.controller = function (app) {
   app.post('/history/outside_transport', (req, res)=>{
         try{
           let sql =
-              `CREATE TABLE ${newTableName}(Trip_No VARCHAR(15) NOT NULL, VehicleNo VARCHAR(15), Make VARCHAR(25), Model VARCHAR(25), Insurance_exp_date Date, PUC_exp_date Date, Material_Type VARCHAR(25),Material VARCHAR(50), Issued_By VARCHAR(50), Issued_Date DATE, Driver_Name VARCHAR(50), Driver_Number BIGINT, Time VARCHAR(15), Consignee_Name VARCHAR(50), Address VARCHAR(150), Gross_Weight DOUBLE, Tare_Weight DOUBLE, Net_Weight DOUBLE, Vehicle_Mapping VARCHAR(15), Qty_Mt_Weight DOUBLE, Status VARCHAR(10), LrNumber VARCHAR(10), LrDate DATE, Card_Number VARCHAR(20),Gross_Wgh_Date_time DATE, Tare_Wgh_Date_time DATE, Gate_In_Date_time DATE, Gate_Out_Date_time DATE. Remark_Field VARCHAR(200), Front_image json, Rear_Image json, PRIMARY KEY(Trip_No))`;
+              `CREATE TABLE ${newTableName}(Trip_No VARCHAR(15) NOT NULL, VehicleNo VARCHAR(15), Make VARCHAR(25), Model VARCHAR(25), Insurance_exp_date Date, PUC_exp_date Date, Material_Type VARCHAR(25),Material VARCHAR(50), Issued_By VARCHAR(50), Issued_Date DATE, Driver_Name VARCHAR(50), Driver_Number BIGINT, Time VARCHAR(15), Consignee_Name VARCHAR(50), Address VARCHAR(150), Gross_Weight DOUBLE, Tare_Weight DOUBLE, Net_Weight DOUBLE, Vehicle_Mapping VARCHAR(15), Qty_Mt_Weight DOUBLE, Status VARCHAR(10), LrNumber VARCHAR(10), LrDate DATE, Card_Number VARCHAR(20),Gross_Wgh_Date_time DATE, Tare_Wgh_Date_time DATE, Gate_In_Date_time DATE, Gate_Out_Date_time DATE, Remark_Field VARCHAR(200), Front_image json, Rear_Image json, PRIMARY KEY(Trip_No))`;
             db.query(sql, (err) => {
               try {
                 if (err) {
